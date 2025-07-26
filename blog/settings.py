e67dd7840eced03c748d55c34cfcab91559b1cba
hello <hello@hellos-MacBook-Pro-2.local>
2025-07-26 12:12:26 +0700

    update settings

--- blog/settings.py
+++ blog/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-v40*=5qv%)q^=sb-kpav+&uvc3!^%8jx%9nu%0_g@%$w^u^^$w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['betatips.com', 'wwww.betatips.com']
-CSRF_TRUSTED_ORIGINS = ['https://betatips.com']
+ALLOWED_HOSTS = ['betatest-1dan.onrender.com', 'wwww.betatest-1dan.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://betatest-1dan.onrender.com.com']
 
 
 # Application definition
